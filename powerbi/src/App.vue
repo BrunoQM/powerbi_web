8abdf1e476c1e0d6cef2cc8b19f03155a62cdaee
BrunoQM <Brunoyt530@gmail.com>
2025-06-10 15:55:17 -0600

    Segundo Commit

--- powerbi/src/App.vue
+++ powerbi/src/App.vue
@@ -6,7 +6,7 @@
 <title>Datamart</title>
     <div class="container" >
         <h1>REPORTE DE POWER BI </h1>
-        <p>Visualizacion interactiva de datos generada en Power BI</p>
+        <p>Visualizacion interactiva de datos generada en PowerBI</p>
         <div class="iframe-container">
     <iframe title="PracticaPoweBI" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiM2YwMDg2MmQtNjc3MS00MTJjLWFjYjctNmMyMGY3MDMzOTAxIiwidCI6IjZmNDBkMjIwLWIwOTEtNGRjMi05NmI5LTZiN2Y5ZGQyZWRmNiJ9" frameborder="0" allowFullScreen="true"></iframe>
 </div>        
